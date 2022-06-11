320d3395b6f85fb18d18c9acd350226b00ac0dd5
dumbu1 <krishnamohan.balivada@gmail.com>
2022-06-11 17:19:08 +0530

    1

---
+++ Re-Run_Fail_TestCases/src/test/java/testRuns/TestRunner_LoginPage.java
@@ -0,0 +1,25 @@
+package testRuns;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		
+		features="src/test/resources/features/homePage",
+		glue = {"stepDefinations","hooks"},
+		
+		plugin = {"rerun:target/failedRerun.txt",
+				"pretty",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+		
+		monochrome = true,
+		dryRun = false
+		
+		)
+
+public class TestRunner_LoginPage {
+
+}
